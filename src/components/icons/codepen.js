3c1276e73155b378a0e96b487eeb8ecaf27c5021
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-03-10 15:47:52 +0600

    first

---
+++ src/components/icons/codepen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const IconCodepen = () => (
+<<<<<<< HEAD
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#ffffff">
+    {' '}
+    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+=======
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-codepen">
+    <title>Codeforces</title>
+    <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
+    <line x1="12" y1="22" x2="12" y2="15.5"></line>
+    <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
+    <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
+    <line x1="12" y1="2" x2="12" y2="8.5"></line>
+>>>>>>> 1adbda0500141b89e515ef3a09fe96def52a4493
+  </svg>
+);
+
+export default IconCodepen;
